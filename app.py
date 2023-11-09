449ed7fb95f875e5c16ff7a06a69945fd4238a77
nils-holmberg <nils.holmberg@outlook.com>
2023-11-09 09:15:31 +0100

    reproducible data analysis on open science framework @ 231109-091526

--- app.py
+++ app.py
@@ -18,18 +18,27 @@ if selected_condition == "a":
 else:
     st.image("img/exps-stim-t.png")
 
-form = st.form("some_form")
-
-option = form.radio(
-    "please look at the advert and rate your attitude towards the product",
-    ["1", "2", "3", "4", "5"],
-    captions = ["strongly negative", "negative", "neither", "positive", "strongly positive"], horizontal=True)
+#form = st.form("some_form")
+#option = form.radio(
+#    "please look at the advert and rate your attitude towards the product",
+#    ["1", "2", "3", "4", "5"],
+#    captions = ["strongly negative", "negative", "neither", "positive", "strongly positive"], horizontal=True)
 
 #if rating == '3':
 #    st.write('selected neither')
 #else:
 #    st.write("negative or positive")
 
+with st.form("my_form"):
+#   st.write("Inside the form")
+#   slider_val = st.slider("Form slider")
+#   checkbox_val = st.checkbox("Form checkbox")
+    option_val = st.radio("please look at the advert and rate your attitude towards the product", ["1", "2", "3", "4", "5"], captions = ["strongly negative", "negative", "neither", "positive", "strongly positive"], horizontal=True)
+
+   # form must have a submit button.
+   submitted = st.form_submit_button("submit")
+   if submitted:
+       st.write("option", option_val)
 
 st.write("check out this link: [osm-py](%s)" % "https://nils-holmberg.github.io/socs-qmd/web/osm-py/about.html")
 
